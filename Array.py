7569551517fa3db331b1e8ae938301745ae7b1ac
proy251183 <partho.ece@gmail.com>
2016-10-02 00:41:39 -0400

    Oct1 new

--- Array.py
+++ Array.py
@@ -19,5 +19,14 @@ class Array(object):
     def __setitem__(self,index, ivalue):
         self._items[index] = ivalue
 
+    def insert(self,index,ivalue):
+        self._intm = []
+        for i in range(0,index-1):
+            self._intm.append(self._items[i])
+        self._intm.append(ivalue)
+        for i in range(index-1,len(self._items)):
+            self._intm.append(self._items[i])
+        self._items = self._intm
+
     
          
---
+++ deomInsertArray.py
@@ -0,0 +1,7 @@
+import os
+os.chdir(r'E:\Python')
+from Array import Array
+a1 = Array(10,7)
+print(a1)
+a1.insert(2,5)
+print(a1)
